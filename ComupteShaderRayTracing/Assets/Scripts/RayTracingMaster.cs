089831d83c6386f5d9ad2f7fc9ade4bb42715c2b
Kamil Chruścinski <kamil.chruscinski@gmail.com>
2020-03-02 21:47:21 +0100

    + Messing with values

--- ComupteShaderRayTracing/Assets/Scripts/RayTracingMaster.cs
+++ ComupteShaderRayTracing/Assets/Scripts/RayTracingMaster.cs
@@ -46,12 +46,16 @@ public class RayTracingMaster : MonoBehaviour
         // Add a number of random spheres
         for (int i = 0; i < SpheresMax; i++)
         {
+
             Sphere sphere = new Sphere();
             // Radius and radius
             sphere.radius = SphereRadius.x + Random.value * (SphereRadius.y - SphereRadius.x);
             Vector2 randomPos = Random.insideUnitCircle * SpherePlacementRadius;
             sphere.position = new Vector3(randomPos.x, sphere.radius, randomPos.y);
-            sphere.emission = new Vector3(0, 0, 0);
+
+            bool emmit = Random.value < 0.5f;
+
+            sphere.emission = emmit ? new Vector3(2f, 2f, 2f) : Vector3.zero;
             sphere.smoothness = randomPos.x;
 
             // Reject spheres that are intersecting others
@@ -63,7 +67,7 @@ public class RayTracingMaster : MonoBehaviour
             }
             // Albedo and specular color
             Color color = Random.ColorHSV();
-            bool metal = Random.value < 0.8f;
+            bool metal = Random.value < 0.5f;
             sphere.albedo = metal ? Vector3.zero : new Vector3(color.r, color.g, color.b);
             sphere.specular = metal ? new Vector3(color.r, color.g, color.b) : Vector3.one * 0.04f;
             // Add the sphere to the list
